45462bbd6cfd5d8009c7074d98ba4f3cf9ff97ee
Dustin <dustin@fulhaus.com>
2021-10-21 12:02:05 -0400

    git init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/index.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App bg-black'>
+      <header className='App-header'>
+        <p>Testing</p>
+        <img src={logo} className='App-logo w-20' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
