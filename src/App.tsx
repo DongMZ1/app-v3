72fae37f022f4ada8c507ab0010433f88c9389a2
Dustin <dustin@fulhaus.com>
2021-10-21 12:15:29 -0400

    made sure tailwind works

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,63 @@ import './styles/index.scss';
 
 const App = () => {
   return (
-    <div className='App bg-black'>
-      <header className='App-header'>
-        <p>Testing</p>
-        <img src={logo} className='App-logo w-20' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='text-center m-5'>Grid system using Tailwind</h1>
+      <div className='flex mb-4'>
+        <div className='w-full bg-gray-300 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/2 bg-gray-500 h-12'></div>
+        <div className='w-1/2 bg-gray-300 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/3 bg-gray-500 h-12'></div>
+        <div className='w-1/3 bg-gray-300 h-12'></div>
+        <div className='w-1/3 bg-gray-500 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/4 bg-gray-300 h-12'></div>
+        <div className='w-1/4 bg-gray-500 h-12'></div>
+        <div className='w-1/4 bg-gray-300 h-12'></div>
+        <div className='w-1/4 bg-gray-500 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/5 bg-gray-300 h-12'></div>
+        <div className='w-1/5 bg-gray-500 h-12'></div>
+        <div className='w-1/5 bg-gray-300 h-12'></div>
+        <div className='w-1/5 bg-gray-500 h-12'></div>
+        <div className='w-1/5 bg-gray-300 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/6 bg-gray-500 h-12'></div>
+        <div className='w-1/6 bg-gray-300 h-12'></div>
+        <div className='w-1/6 bg-gray-500 h-12'></div>
+        <div className='w-1/6 bg-gray-300 h-12'></div>
+        <div className='w-1/6 bg-gray-500 h-12'></div>
+        <div className='w-1/6 bg-gray-300 h-12'></div>
+      </div>
+      <div className='flex'>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+      </div>
+
+      <h2>Card</h2>
+    </>
   );
 };
 
