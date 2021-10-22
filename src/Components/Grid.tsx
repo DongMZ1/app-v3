06c156a21a1b9f833e289c05fcc2b6de64f902e6
Dustin <dustin@fulhaus.com>
2021-10-21 21:38:20 -0400

    added three tailwind examples

---
+++ src/Components/Grid.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const Grid = (): JSX.Element => {
+  return (
+    <>
+      <h1 className='text-center m-5'>Grid system using Tailwind</h1>
+      <div className='flex mb-4'>
+        <div className='w-full bg-gray-300 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/2 bg-gray-500 h-12'></div>
+        <div className='w-1/2 bg-gray-300 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/3 bg-gray-500 h-12'></div>
+        <div className='w-1/3 bg-gray-300 h-12'></div>
+        <div className='w-1/3 bg-gray-500 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/4 bg-gray-300 h-12'></div>
+        <div className='w-1/4 bg-gray-500 h-12'></div>
+        <div className='w-1/4 bg-gray-300 h-12'></div>
+        <div className='w-1/4 bg-gray-500 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/5 bg-gray-300 h-12'></div>
+        <div className='w-1/5 bg-gray-500 h-12'></div>
+        <div className='w-1/5 bg-gray-300 h-12'></div>
+        <div className='w-1/5 bg-gray-500 h-12'></div>
+        <div className='w-1/5 bg-gray-300 h-12'></div>
+      </div>
+
+      <div className='flex mb-4'>
+        <div className='w-1/6 bg-gray-500 h-12'></div>
+        <div className='w-1/6 bg-gray-300 h-12'></div>
+        <div className='w-1/6 bg-gray-500 h-12'></div>
+        <div className='w-1/6 bg-gray-300 h-12'></div>
+        <div className='w-1/6 bg-gray-500 h-12'></div>
+        <div className='w-1/6 bg-gray-300 h-12'></div>
+      </div>
+      <div className='flex'>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+        <div className='w-1/12 bg-gray-500 h-12'></div>
+        <div className='w-1/12 bg-gray-300 h-12'></div>
+      </div>
+    </>
+  );
+};
+
+export default Grid;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,61 +1,12 @@
 import './HomePage.scss';
+import { Hero, Menu, Grid } from '../../Components';
 
 const Home = (): JSX.Element => {
   return (
     <>
-      <h1 className='text-center m-5'>Grid system using Tailwind</h1>
-      <div className='flex mb-4'>
-        <div className='w-full bg-gray-300 h-12'></div>
-      </div>
-
-      <div className='flex mb-4'>
-        <div className='w-1/2 bg-gray-500 h-12'></div>
-        <div className='w-1/2 bg-gray-300 h-12'></div>
-      </div>
-
-      <div className='flex mb-4'>
-        <div className='w-1/3 bg-gray-500 h-12'></div>
-        <div className='w-1/3 bg-gray-300 h-12'></div>
-        <div className='w-1/3 bg-gray-500 h-12'></div>
-      </div>
-
-      <div className='flex mb-4'>
-        <div className='w-1/4 bg-gray-300 h-12'></div>
-        <div className='w-1/4 bg-gray-500 h-12'></div>
-        <div className='w-1/4 bg-gray-300 h-12'></div>
-        <div className='w-1/4 bg-gray-500 h-12'></div>
-      </div>
-
-      <div className='flex mb-4'>
-        <div className='w-1/5 bg-gray-300 h-12'></div>
-        <div className='w-1/5 bg-gray-500 h-12'></div>
-        <div className='w-1/5 bg-gray-300 h-12'></div>
-        <div className='w-1/5 bg-gray-500 h-12'></div>
-        <div className='w-1/5 bg-gray-300 h-12'></div>
-      </div>
-
-      <div className='flex mb-4'>
-        <div className='w-1/6 bg-gray-500 h-12'></div>
-        <div className='w-1/6 bg-gray-300 h-12'></div>
-        <div className='w-1/6 bg-gray-500 h-12'></div>
-        <div className='w-1/6 bg-gray-300 h-12'></div>
-        <div className='w-1/6 bg-gray-500 h-12'></div>
-        <div className='w-1/6 bg-gray-300 h-12'></div>
-      </div>
-      <div className='flex'>
-        <div className='w-1/12 bg-gray-500 h-12'></div>
-        <div className='w-1/12 bg-gray-300 h-12'></div>
-        <div className='w-1/12 bg-gray-500 h-12'></div>
-        <div className='w-1/12 bg-gray-300 h-12'></div>
-        <div className='w-1/12 bg-gray-500 h-12'></div>
-        <div className='w-1/12 bg-gray-300 h-12'></div>
-        <div className='w-1/12 bg-gray-500 h-12'></div>
-        <div className='w-1/12 bg-gray-300 h-12'></div>
-        <div className='w-1/12 bg-gray-500 h-12'></div>
-        <div className='w-1/12 bg-gray-300 h-12'></div>
-        <div className='w-1/12 bg-gray-500 h-12'></div>
-        <div className='w-1/12 bg-gray-300 h-12'></div>
-      </div>
+      <Menu />
+      <Hero />
+      <Grid />
     </>
   );
 };
