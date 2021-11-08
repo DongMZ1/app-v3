2f4c5eafbd873c89cde0cdbd98bf254ff716e419
DongMZ1 <915094594@qq.com>
2021-11-08 14:50:03 -0500

    input icon not find

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,10 @@ import { ReactComponent as FulhausIcon } from "../../styles/images/fulhaus.svg";
 import { ReactComponent as ShareAlt } from "../../styles/images/share-alt.svg";
 import {ReactComponent as HomePageExit} from '../../styles/images/home-page-exit.svg'
 import {ResponsiveLink} from '@fulhaus/react.ui.responsive-link'
+import {Button} from '@fulhaus/react.ui.button'
+import {TextInput} from '@fulhaus/react.ui.text-input';
 const Home = (): JSX.Element => {
+  const [searchkeyWord, setsearchKeyWord] = useState('');
   return (
     <div className="app-v3-home-page">
       <div className="px-8 py-4 flex bg-white">
@@ -16,6 +19,7 @@ const Home = (): JSX.Element => {
       <div className='mt-10 mx-5percent bg-second'>
             <div className='flex justify-between'>
               <div className='text-4xl moret'>SELECT A PROJECT</div>
+              <Button className='my-2'>Start a new project</Button>
             </div>
       </div>
     </div>
