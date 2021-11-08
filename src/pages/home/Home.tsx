d27e99118dbf1c6e5010dc942b830da0050595b9
DongMZ1 <915094594@qq.com>
2021-11-08 15:09:31 -0500

    ui finished, still need to figure out tailwind

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,26 +2,45 @@ import "./HomePage.scss";
 import React, { useState } from "react";
 import { ReactComponent as FulhausIcon } from "../../styles/images/fulhaus.svg";
 import { ReactComponent as ShareAlt } from "../../styles/images/share-alt.svg";
-import {ReactComponent as HomePageExit} from '../../styles/images/home-page-exit.svg'
-import {ResponsiveLink} from '@fulhaus/react.ui.responsive-link'
-import {Button} from '@fulhaus/react.ui.button'
-import {TextInput} from '@fulhaus/react.ui.text-input';
+import { ReactComponent as HomePageExit } from "../../styles/images/home-page-exit.svg";
+import { ReactComponent as HomePageEmptyCover } from "../../styles/images/home-page-empty-cover.svg";
+import { ResponsiveLink } from "@fulhaus/react.ui.responsive-link";
+import { Button } from "@fulhaus/react.ui.button";
+import { TextInput } from "@fulhaus/react.ui.text-input";
 const Home = (): JSX.Element => {
-  const [searchkeyWord, setsearchKeyWord] = useState('');
+  const [searchkeyWord, setsearchKeyWord] = useState("");
   return (
     <div className="app-v3-home-page">
       <div className="px-8 py-4 flex bg-white">
         <FulhausIcon />
-        <ShareAlt className='ml-auto my-auto mr-4 cursor-pointer' />
-        <ResponsiveLink className='my-auto font-semibold text-sm border-solid border-black border-b mr-8' url=''>Invite people</ResponsiveLink>
-        <HomePageExit className='my-auto cursor-pointer' />
+        <ShareAlt className="ml-auto my-auto mr-4 cursor-pointer" />
+        <ResponsiveLink
+          className="my-auto font-semibold text-sm border-solid border-black border-b mr-8"
+          url=""
+        >
+          Invite people
+        </ResponsiveLink>
+        <HomePageExit className="my-auto cursor-pointer" />
       </div>
-      <div className='mt-10 mx-5percent bg-second'>
-            <div className='flex justify-between'>
-              <div className='text-4xl moret'>SELECT A PROJECT</div>
-              <Button className='my-2'>Start a new project</Button>
-            </div>
+      <div className="mt-10 mx-8percent bg-second">
+        <div className="flex justify-between">
+          <div className="text-4xl moret">SELECT A PROJECT</div>
+          <Button className="my-2">Start a new project</Button>
+        </div>
       </div>
+      <div className="mx-8percent">
+        <TextInput
+          variant="box"
+          className='mt-6 text-xs'
+          inputName="email"
+          type="search"
+          value={searchkeyWord}
+          placeholder='Search for a project name'
+          onChange={(e) => setsearchKeyWord((e.target as any).value)}
+        />
+      </div>
+      <div className='flex'><HomePageEmptyCover className='mx-auto' /></div>
+      <div className='flex'><div className='text-3xl moret mx-auto'>Get started with a new project</div></div>
     </div>
   );
 };
