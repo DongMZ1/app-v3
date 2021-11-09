01277eba625832fb735dd3c15acaa0b835e2ac63
DongMZ1 <915094594@qq.com>
2021-11-09 15:32:35 -0500

    add history

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -10,13 +10,15 @@ import { ReactComponent as ExitIcon } from "../../styles/images/home-page-exit.s
 
 import Quote from './Quote/Quote';
 import Design from './Design/Design';
+import { ClickOutsideAnElementHandler } from '@fulhaus/react.ui.click-outside-an-element-handler';
 
 const Project = () => {
     const [QuoteOrDesign, setQuoteOrDesign] = useState<'Quote' | 'Design'>('Quote')
+    const [showHistory, setshowHistory] = useState(false);
     return (
         <div className="project bg-sand">
-            <div className="flex bg-white">
-                <div className='flex w-1/6 px-4 py-3 text-white bg-black yx-4 font-moret'>
+            <div className="flex bg-white h-14">
+                <div className='flex w-1/6 px-4 py-3 text-white bg-black font-moret'>
                     <Link className='my-auto cursor-pointer' to={'/'}><RightArrowWhiteIcon /></Link>
                     <div className='m-auto text-lg'>PROJECT NAME</div>
                     <div className='my-auto cursor-pointer'>···</div>
@@ -30,11 +32,12 @@ const Project = () => {
                       <div className='my-auto mr-8 text-sm font-ssp'>Never saved</div>
                       <ShareAlt className='my-auto mr-8 cursor-pointer' />
                       <InformationIcon className='my-auto mr-8 cursor-pointer' />
-                      <HistoryIcon className='my-auto mr-8 cursor-pointer' />
+                      <HistoryIcon onClick={()=>setshowHistory(true)} className='my-auto mr-8 cursor-pointer' />
                       <SaveIcon className='my-auto mr-8 cursor-pointer' />
                       <ExitIcon className='my-auto mr-10 cursor-pointer' />
                 </div>
             </div>
+            {showHistory && <ClickOutsideAnElementHandler onClickedOutside={()=>setshowHistory(false)}><div className='fixed right-0 z-10 w-1/4 h-full border border-black border-solid bg-sand'>history</div></ClickOutsideAnElementHandler>}
             {QuoteOrDesign === 'Quote' && <Quote />}
             {QuoteOrDesign === 'Design' && <Design />}
         </div>
