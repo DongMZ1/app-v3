e226c6c4106cd055aea0e02fbf2ae9dc8c01f3b0
DongMZ1 <915094594@qq.com>
2021-11-10 14:32:26 -0500

    edit notes finished

--- src/pages/project/components/ProjectInformation.tsx
+++ src/pages/project/components/ProjectInformation.tsx
@@ -4,11 +4,11 @@ import { ReactComponent as ExitIcon } from '../../../styles/images/exit.svg'
 import { ReactComponent as CopyIcon } from '../../../styles/images/copy-black.svg'
 import { ReactComponent as EditPenIcon } from '../../../styles/images/edit-pen.svg'
 import { ReactComponent as FilesIcon } from '../../../styles/images/files-black.svg'
-import { DropdownListInput } from '@fulhaus/react.ui.dropdown-list-input'
+import { DropdownListInput } from '@fulhaus/react.ui.dropdown-list-input';
+
 type ProjectInformationType = {
     close: () => void
 }
-
 
 const ProjectInformation = ({ close }: ProjectInformationType) => {
     const [CopiedQuoteID, setCopiedQuoteID] = useState(false);
@@ -21,74 +21,84 @@ const ProjectInformation = ({ close }: ProjectInformationType) => {
     const [shippingUnit, setshippingUnit] = useState('CAD')
     const [shippingValue, setshippingValue] = useState<number | undefined>();
 
-    return <div className='fixed right-0 z-10 w-5/12 h-full overflow-auto border-l border-black border-solid bg-sand project-information'>
-        <div className='flex'>
-            <div className='text-2xl uppercase font-moret'>project information</div>
-            <ExitIcon onClick={() => close()} className='my-auto ml-auto cursor-pointer' />
-        </div>
-        <div className='flex mt-2'>
-            <div className='my-auto mr-8 text-sm font-ssp'>Quote ID: 45999DF</div>
-            {CopiedQuoteID ? <div className='my-auto mr-4 text-sm font-semibold font-ssp'>Copied!</div> : <CopyIcon onClick={() => {
-                navigator.clipboard.writeText('this is a copy icon');
-                setCopiedQuoteID(true);
-                setTimeout(() => setCopiedQuoteID(false), 500);
-            }} className='my-auto mr-4 cursor-pointer' />}
-            <a className='text-sm border-b border-solid cursor-pointer font-ssp text-link border-link'>View on Pipedrive</a>
-        </div>
-        <div className='flex mt-8'>
-            <div className='my-auto mr-6 text-sm font-semibold font-ssp'>Currency:</div>
-            <div className='w-20'>
-                <DropdownListInput
-                    initialValue={currency}
-                    onSelect={(value) => setcurrency(value)}
-                    options={['CAD', 'USD', 'EURO']} />
+    const [editNotes, seteditNotes] = useState(false);
+    const [notesContent, setnotesContent] = useState('Dexter "The Blade" Jackson (born November 25, 1969) is a retired American IFBB professional bodybuilder and the 2008 Mr. Olympia bodybuilding champion.');
+
+    return (
+        <div className='fixed right-0 z-10 w-5/12 h-full overflow-auto border-l border-black border-solid bg-sand project-information'>
+            <div className='flex'>
+                <div className='text-2xl uppercase font-moret'>project information</div>
+                <ExitIcon onClick={() => close()} className='my-auto ml-auto cursor-pointer' />
             </div>
-        </div>
-        <div className='flex mt-8'>
-            <div className='my-auto mr-6 text-sm font-semibold font-ssp'>Design Style</div>
-            <EditPenIcon className='my-auto ml-auto cursor-pointer' />
-        </div>
-        <div className='mt-8'>
-            <div className='my-auto mr-6 text-sm font-semibold font-ssp'>Price modifiers</div>
-            <div className='flex mt-4'>
-                <div className='w-32 mr-8 font-ssp'>
-                    <div className='mb-1 text-xs'>Discount Code</div>
+            <div className='flex mt-2'>
+                <div className='my-auto mr-8 text-sm font-ssp'>Quote ID: 45999DF</div>
+                {CopiedQuoteID ? <div className='my-auto mr-4 text-sm font-semibold font-ssp'>Copied!</div> : <CopyIcon onClick={() => {
+                    navigator.clipboard.writeText('this is a copy icon');
+                    setCopiedQuoteID(true);
+                    setTimeout(() => setCopiedQuoteID(false), 500);
+                }} className='my-auto mr-4 cursor-pointer' />}
+                <a className='text-sm border-b border-solid cursor-pointer font-ssp text-link border-link'>View on Pipedrive</a>
+            </div>
+            <div className='flex mt-8'>
+                <div className='my-auto mr-6 text-sm font-semibold font-ssp'>Currency:</div>
+                <div className='w-20'>
                     <DropdownListInput
-                        onSelect={(value) => setdiscountCode(value)}
-                        options={['Premo1', 'Premo2', 'Premo3']} />
+                        initialValue={currency}
+                        onSelect={(value) => setcurrency(value)}
+                        options={['CAD', 'USD', 'EURO']} />
                 </div>
-                <div className='w-40 mr-8 font-ssp'>
-                    <div className='mb-1 text-xs'>Installation Fee</div>
-                    <div className='flex drop-down-bg-sand'>
+            </div>
+            <div className='flex mt-8'>
+                <div className='my-auto mr-6 text-sm font-semibold font-ssp'>Design Style</div>
+                <EditPenIcon className='my-auto ml-auto cursor-pointer' />
+            </div>
+            <div className='mt-8'>
+                <div className='my-auto mr-6 text-sm font-semibold font-ssp'>Price modifiers</div>
+                <div className='flex mt-4'>
+                    <div className='w-32 mr-8 font-ssp'>
+                        <div className='mb-1 text-xs'>Discount Code</div>
                         <DropdownListInput
-                            initialValue={installationUnit}
-                            onSelect={value => setinstallationUnit(value)}
-                            options={['%', 'CAD', 'USD', 'EURO']} />
-                        <input type='number' value={installationValue} onChange={(e) => setinstallationValue(e.target.valueAsNumber)} className='w-20 pl-1 text-xs border border-black border-solid' />
+                            onSelect={(value) => setdiscountCode(value)}
+                            options={['Premo1', 'Premo2', 'Premo3']} />
                     </div>
-                </div>
-                <div className='w-40 font-ssp'>
-                    <div className='mb-1 text-xs'>Shipping Fee</div>
-                    <div className='flex drop-down-bg-sand'>
-                        <DropdownListInput
-                            initialValue={shippingUnit}
-                            onSelect={(value) => setshippingUnit(value)}
-                            options={['CAD', 'USD', 'EURO']} />
-                        <div className='flex bg-white border-t border-b border-black border-solid'><div className='my-auto'>$</div></div>
-                        <input className='w-20 text-xs border-t border-b border-r border-black border-solid' />
+                    <div className='w-40 mr-8 font-ssp'>
+                        <div className='mb-1 text-xs'>Installation Fee</div>
+                        <div className='flex drop-down-bg-sand'>
+                            <DropdownListInput
+                                initialValue={installationUnit}
+                                onSelect={value => setinstallationUnit(value)}
+                                options={['%', 'CAD', 'USD', 'EURO']} />
+                            <input type='number' value={installationValue} onChange={(e) => setinstallationValue(e.target.valueAsNumber)} className='w-20 pl-1 text-xs border border-black border-solid' />
+                        </div>
+                    </div>
+                    <div className='w-40 font-ssp'>
+                        <div className='mb-1 text-xs'>Shipping Fee</div>
+                        <div className='flex drop-down-bg-sand'>
+                            <DropdownListInput
+                                initialValue={shippingUnit}
+                                onSelect={(value) => setshippingUnit(value)}
+                                options={['CAD', 'USD', 'EURO']} />
+                            <div className='flex bg-white border-t border-b border-black border-solid'><div className='my-auto'>$</div></div>
+                            <input type='number' value={shippingValue} onChange={(e) => setshippingValue(e.target.valueAsNumber)} className='w-20 text-xs border-t border-b border-r border-black border-solid' />
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className='flex mt-8'>
+                <div className='my-auto text-sm font-semibold font-ssp'>Files</div>
+                <FilesIcon className='my-auto ml-auto cursor-pointer' />
+            </div>
+            <div className='flex mt-6 mb-4'>
+                <div className='my-auto text-sm font-semibold font-ssp'>Notes</div>
+                <EditPenIcon onClick={() => { if (editNotes) { seteditNotes(false) } else { seteditNotes(true) } }} className='my-auto ml-auto cursor-pointer' />
+            </div>
+            {
+                editNotes ? <textarea className='w-full p-1 text-sm border border-black border-solid font-ssp' onBlur={() => {
+                    seteditNotes(false);
+                }} onChange={e => setnotesContent(e.target.value)} value={notesContent} /> : <div className='text-sm font-ssp'>{notesContent}</div>
+            }
         </div>
-        <div className='flex mt-8'>
-            <div className='my-auto text-sm font-semibold font-ssp'>Files</div>
-            <FilesIcon className='my-auto ml-auto cursor-pointer' />
-        </div>
-        <div className='flex mt-6'>
-            <div className='my-auto text-sm font-semibold font-ssp'>Notes</div>
-            <EditPenIcon className='my-auto ml-auto cursor-pointer' />
-        </div>
-    </div>
+    )
 }
 
 export default ProjectInformation;
