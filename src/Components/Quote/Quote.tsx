4e25411d49d23c72073772d18048c000c8e76712
DongMZ1 <915094594@qq.com>
2022-01-13 14:52:07 -0500

    add restrict to add rooms

--- src/Components/Quote/Quote.tsx
+++ src/Components/Quote/Quote.tsx
@@ -7,6 +7,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Tappstate } from '../../redux/reducers';
 import apiRequest from '../../Service/apiRequest';
 const Quote = () => {
+    const userRole = useSelector((state: Tappstate) => state.selectedProject)?.userRole;
     const quoteUnitLength = useSelector((state: Tappstate) => state.quoteDetail)?.data?.length;
     const selectedQuoteUnit = useSelector((state: Tappstate) => state.selectedQuoteUnit);
     const currentOrgID = useSelector((state: Tappstate) => state.currentOrgID);
@@ -20,43 +21,45 @@ const Quote = () => {
             body: { roomType: room },
             method: 'POST'
         })
-        if(res?.success){
-            const newselectedQuoteUnit = produce(selectedQuoteUnit, (draft:any) => {
+        if (res?.success) {
+            const newselectedQuoteUnit = produce(selectedQuoteUnit, (draft: any) => {
                 draft.rooms = draft.rooms?.concat(res.newRoom);
             });
             //update selectedQuoteUnit
             dispatch({
-                type:'selectedQuoteUnit',
-                payload:newselectedQuoteUnit
+                type: 'selectedQuoteUnit',
+                payload: newselectedQuoteUnit
             });
             //update quoteDetail
-            const newquoteDetail = produce(quoteDetail, (draft:any) => {
-                const index = draft.data.findIndex((each:any) => each?.unitID === unitID)
+            const newquoteDetail = produce(quoteDetail, (draft: any) => {
+                const index = draft.data.findIndex((each: any) => each?.unitID === unitID)
                 draft.data[index] = newselectedQuoteUnit;
             });
             dispatch({
-                type:'quoteDetail',
-                payload:newquoteDetail
+                type: 'quoteDetail',
+                payload: newquoteDetail
             });
-        }else{
+        } else {
             console.log(res?.message)
         }
     }
     return <div className='flex flex-col w-full h-full px-6 pt-4 overflow-y-auto quote'>
         {(selectedQuoteUnit) ?
             <>
-                <div className='flex'>
-                    <div className='w-32 mr-8 text-sm-important'>
-                        <DropdownListInput prefixIcon={<div className='flex'><div className='m-auto'>Add Room</div></div>} wrapperClassName='cursor-pointer dropdown-list-input-box-display-none' listWrapperClassName='width-52-important'
-                            onSelect={(v) => addRoom(v)}
-                            options={['bedroom', 'dining room', 'bathroom', 'living room', 'accessories', 'pillow set']} />
+                { userRole !=='viewer' &&
+                    <div className='flex'>
+                        <div className='w-32 mr-8 text-sm-important'>
+                            <DropdownListInput prefixIcon={<div className='flex'><div className='m-auto'>Add Room</div></div>} wrapperClassName='cursor-pointer dropdown-list-input-box-display-none' listWrapperClassName='width-52-important'
+                                onSelect={(v) => addRoom(v)}
+                                options={['bedroom', 'dining room', 'bathroom', 'living room', 'accessories', 'pillow set']} />
+                        </div>
+                        <div className='w-60 text-sm-important'>
+                            <DropdownListInput placeholder='Add Muti-Room Package' wrapperClassName='cursor-pointer' listWrapperClassName='' options={['Custom Unit', 'Studio', '1BR', '2BR', '3BR', '1BR-HOTEL']} />
+                        </div>
                     </div>
-                    <div className='w-60 text-sm-important'>
-                        <DropdownListInput placeholder='Add Muti-Room Package' wrapperClassName='cursor-pointer' listWrapperClassName='' options={['Custom Unit', 'Studio', '1BR', '2BR', '3BR', '1BR-HOTEL']} />
-                    </div>
-                </div>
+                }
                 {
-                    selectedQuoteUnit?.rooms?.map((each:any) => <Room eachRoom={each} />)
+                    selectedQuoteUnit?.rooms?.map((each: any) => <Room eachRoom={each} />)
                 }
             </>
             :
