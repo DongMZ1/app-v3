22d4d6081d4d8134d8edfe2c1896a0f9122429f6
DongMZ1 <915094594@qq.com>
2022-01-31 14:16:31 -0500

    footer changed

--- src/pages/project/ProjectComponents/ProjectFooter.tsx
+++ src/pages/project/ProjectComponents/ProjectFooter.tsx
@@ -3,6 +3,7 @@ import { Tappstate } from "../../../redux/reducers";
 import { useSelector } from "react-redux";
 import apiRequest from '../../../Service/apiRequest';
 import debounce from 'lodash.debounce';
+import { useHistory } from 'react-router';
 const ProjectFooter = () => {
     const [unitTotal, setunitTotal] = useState(0);
     const [quoteTotal, setquoteTotal] = useState(0);
@@ -11,6 +12,7 @@ const ProjectFooter = () => {
     const currentOrgID = useSelector((state: Tappstate) => state.currentOrgID)
     const JSONquoteDetail = JSON.stringify(quoteDetail);
     const JSONselectedQuoteUnit = JSON.stringify(selectedQuoteUnit);
+    const history = useHistory();
     useEffect(
         () => {
             const getQuoteTotal = async () => {
@@ -59,7 +61,7 @@ const ProjectFooter = () => {
         <div className='my-auto text-lg font-semibold'>{selectedQuoteUnit ? selectedQuoteUnit.count : '0'} Unit</div>
         <div className='my-auto ml-auto mr-6 text-sm'>Unit Total <b>{selectedQuoteUnit? `$${unitTotal.toFixed(2)}`: 'No Unit Selected'}</b></div>
         <div className='my-auto mr-6 text-sm'>Project Total <b>${quoteTotal? quoteTotal.toFixed(2) : 0}</b></div>
-        <div className='px-4 py-1 my-auto mr-6 text-sm font-semibold bg-black cursor-pointer'>View Overall Budget</div>
+        <div onClick={()=>history.push('/quote-summary-rental')} className='px-4 py-1 my-auto mr-6 text-sm font-semibold bg-black cursor-pointer'>View Overall Budget</div>
     </div>
 }
 
