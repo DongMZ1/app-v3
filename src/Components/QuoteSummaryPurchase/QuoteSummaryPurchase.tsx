6774401a2176d87e703fca71df053090434752ec
DongMZ1 <915094594@qq.com>
2022-02-02 17:02:01 -0500

    one specific unit budget page finished

--- src/Components/QuoteSummaryPurchase/QuoteSummaryPurchase.tsx
+++ src/Components/QuoteSummaryPurchase/QuoteSummaryPurchase.tsx
@@ -1,6 +1,12 @@
 import './QuoteSummaryPurchase.scss'
-
+import UnitBudget from '../UnitBudget/UnitBudget'
+import { useSelector } from 'react-redux'
+import { Tappstate } from '../../redux/reducers'
 const QuoteSummaryPurchase = () => {
+    const selectedQuoteUnit = useSelector((state:Tappstate) => state.selectedQuoteUnit);
+    if(selectedQuoteUnit){
+        return <UnitBudget />
+    }
     return <div className='flex flex-col w-full h-full px-6 py-4 overflow-y-auto quote-summary-purchase'>
 
     </div>
--- src/Components/ViewBudget/ViewBudget.tsx
+++
@@ -1,8 +0,0 @@
-import './ViewBudget.scss'
-const ViewBudget = () => {
-    return <div>
-
-    </div>
-}
-
-export default ViewBudget;
