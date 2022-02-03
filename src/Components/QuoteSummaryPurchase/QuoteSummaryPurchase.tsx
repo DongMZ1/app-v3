b6a0820137ed1d2b36770e2c5e54001e0bdfa77d
DongMZ1 <915094594@qq.com>
2022-02-03 14:39:04 -0500

    add calendar to quote summary page

--- src/Components/QuoteSummaryPurchase/QuoteSummaryPurchase.tsx
+++ src/Components/QuoteSummaryPurchase/QuoteSummaryPurchase.tsx
@@ -1,14 +1,46 @@
+import { useState } from 'react';
 import './QuoteSummaryPurchase.scss'
 import UnitBudget from '../UnitBudget/UnitBudget'
 import { useSelector } from 'react-redux'
 import { Tappstate } from '../../redux/reducers'
+import { ReactComponent as EditPenIcon } from '../../styles/images/edit-pen.svg'
+import { DatePicker } from '@fulhaus/react.ui.date-picker'
+import { FaRegCalendarAlt } from 'react-icons/fa'
+import { ClickOutsideAnElementHandler } from '@fulhaus/react.ui.click-outside-an-element-handler';
 const QuoteSummaryPurchase = () => {
-    const selectedQuoteUnit = useSelector((state:Tappstate) => state.selectedQuoteUnit);
-    if(selectedQuoteUnit){
+    const [editable, seteditable] = useState(false);
+    const [showCalendar, setshowCalendar] = useState(false);
+    const selectedQuoteUnit = useSelector((state: Tappstate) => state.selectedQuoteUnit);
+    if (selectedQuoteUnit) {
         return <UnitBudget />
     }
-    return <div className='flex flex-col w-full h-full px-6 py-4 overflow-y-auto quote-summary-purchase'>
-
+    return <div className='flex flex-col w-full h-full px-6 py-4 overflow-y-auto quote-summary-rental'>
+        <div className='flex'>
+            <div className='my-auto text-2xl font-moret'>Payment Terms</div>
+            {
+                !editable && <>
+                    <div className='my-auto ml-auto mr-8 text-sm font-ssp'>Start Date: May 1, 2021</div>
+                    <EditPenIcon onClick={() => seteditable(true)} className='my-auto cursor-pointer' />
+                </>
+            }
+            {
+                editable && <>
+                    <div className='flex h-full px-4 ml-auto border border-black border-solid '><div className='m-auto'>Start Date</div></div>
+                    <div className='relative flex px-4 bg-white border border-black border-solid'>
+                        <div className='my-auto mr-4'>May 1, 2021</div>
+                        <FaRegCalendarAlt onClick={() => setshowCalendar(true)} className='my-auto cursor-pointer' />
+                        {showCalendar &&
+                            <ClickOutsideAnElementHandler onClickedOutside={() => setshowCalendar(false)}>
+                                <div className='absolute right-0 z-50 p-4 bg-white border border-black border-solid top-full w-80'>
+                                    <DatePicker onSelectDate={(selectedDate) =>
+                                        setshowCalendar(false)
+                                    } />
+                                </div>
+                            </ClickOutsideAnElementHandler>}
+                    </div>
+                </>
+            }
+        </div>
     </div>
 }
 
