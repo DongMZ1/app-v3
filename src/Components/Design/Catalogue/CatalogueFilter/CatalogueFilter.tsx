8e5049ca4b13dd7440943d3d2b3996a08485b71b
DongMZ1 <915094594@qq.com>
2022-02-08 16:32:07 -0500

    continue refactoring Catalogue

--- src/Components/Design/Catalogue/CatalogueFilter/CatalogueFilter.tsx
+++ src/Components/Design/Catalogue/CatalogueFilter/CatalogueFilter.tsx
@@ -12,17 +12,15 @@ import './CatalogueFilter.scss'
 
 import CatalogueFilterRoomAndStyle from './CatalogueFilterRoomAndStyle';
 import CatelogueFilterItemType from './CatelogueFilterItemType';
-//Source & Availability options
-const sourceOptions = ['Vendor Data', 'Clipped Items', 'Starred / Favourited'];
-const availabilityOptions = ['In Stock', 'Restocking', 'Out of Stock', 'Unknown'];
-//Item type
+import CatalogueFilterColor from './CatalogueFilterColor';
+import CatalogueFilterSource from './CatalogueFilterSource';
 
 const CatalogueFilter = () => {
-    //Rooms and Styles states
+    //Rooms and Styles states => CatalogueFilterRoomAndStyle.tsx
     const [showRoomsAndStyle, setshowRoomsAndStyle] = useState(false);
     const [roomsAndStyleRoom, setroomsAndStyleRoom] = useState<string[]>([]);
     const [roomsAndStyleCollections, setroomsAndStyleCollections] = useState<string[]>([]);
-    //Source & Availability state
+    //Source & Availability state => CatalogueFilterSource.tsx
     const [showSourceAvailability, setshowSourceAvailability] = useState(false);
     const [source, setsource] = useState<string[]>([]);
     const [availability, setavailability] = useState<string[]>([]);
@@ -32,11 +30,11 @@ const CatalogueFilter = () => {
     const [minPrice, setminPrice] = useState(0);
     const [maxPrice, setmaxPrice] = useState(3200);
 
-    //Colour
+    //Colour => CatalogueFilterColor.tsx
     const [showColorMenu, setshowColorMenu] = useState(false);
     const [showOtherColorMenu, setshowOtherColorMenu] = useState(false);
 
-    //itemType
+    //itemType => CatelogueFilterItemType.tsx
     const [showItemType, setshowItemType] = useState(false);
     const [itemTypes, setitemTypes] = useState<string[]>([]);
     const [lengthUnit, setlengthUnit] = useState('inch');
@@ -82,105 +80,23 @@ const CatalogueFilter = () => {
                 <div className='w-24 mr-4'>
                     <div className='flex justify-between w-full px-1 text-sm border border-black border-solid cursor-pointer select-none'><div className='my-1'>Vendors</div><BsChevronDown className='my-auto' /></div>
                 </div>
-                {/*------------------------------------------------------Item type------------------------------------------------*/}
-                {/**--------------------------------------COLOUR-------------------------------------------------------- */}
-                <div className='w-1/6 mr-4'>
-                    <div onClick={() => setshowColorMenu(true)} className='flex justify-between w-full px-1 text-sm border border-black border-solid cursor-pointer select-none'><div className='my-1'>Colour</div><BsChevronDown className='my-auto' /></div>
-                    {
-                        showColorMenu && <ClickOutsideAnElementHandler onClickedOutside={() => setshowColorMenu(false)}>
-                            <div className='absolute z-50 px-4 bg-white border border-black border-solid w-200px'>
-                                {
-                                    ['red', 'blue', 'orange', 'black', 'grey'].map(each =>
-                                        <div className='flex px-1 py-2 cursor-pointer'>
-                                            <div style={{ borderRadius: '50%', backgroundColor: each }} className='w-2 h-2 my-auto mr-4'></div>
-                                            <div className='my-auto text-sm font-ssp text-secondary'>{each}</div>
-                                        </div>
-                                    )
-                                }
-                                <div className='flex px-1 py-2 cursor-pointer' onClick={() => {
-                                    setshowColorMenu(false);
-                                    setshowOtherColorMenu(true);
-                                }}>
-                                    <div style={{ borderRadius: '50%', background: 'conic-gradient(from 180deg at 50% 50%, #FF0000 0deg, #FFB800 68.86deg, #FFF500 136.36deg, #05FF00 198.24deg, #00F0FF 245.12deg, #000AFF 301.37deg, #FA00FF 360deg)' }} className='w-2 h-2 my-auto mr-4'></div>
-                                    <div className='my-auto text-sm font-ssp text-secondary'>other</div>
-                                    <AiOutlineRight className='my-auto ml-auto' />
-                                </div>
-                            </div>
-                        </ClickOutsideAnElementHandler>
-                    }
-                    {
-                        showOtherColorMenu && <ClickOutsideAnElementHandler onClickedOutside={() => setshowOtherColorMenu(false)}>
-                            <div className='absolute z-50 px-4 py-4 bg-white border border-black border-solid w-200px'>
-                                <div className='flex mb-2'>
-                                    <FiArrowLeft onClick={() => {
-                                        setshowColorMenu(true);
-                                        setshowOtherColorMenu(false);
-                                    }} className='my-auto mr-4 cursor-pointer' />
-                                    <div className='my-auto text-sm font-semibold font-ssp'>Other Colour</div>
-                                </div>
-                                <ColorPicker />
-                                <div className='flex justify-around mt-4'>
-                                    <Button onClick={() => setshowOtherColorMenu(false)} variant='secondary' className='w-24 ml-auto mr-4'>Cancel</Button>
-                                    <Button className='w-24'>Apply</Button>
-                                </div>
-                            </div>
-                        </ClickOutsideAnElementHandler>
-                    }
-                </div>
+                <CatalogueFilterColor
+                showColorMenu={showColorMenu}
+                setshowColorMenu={setshowColorMenu}
+                showOtherColorMenu={showOtherColorMenu}
+                setshowOtherColorMenu ={setshowOtherColorMenu }
+                />
             </div>
             <div className="flex mt-4 dropdown-component-overwrite">
                 {/**--------------------------------source and availability--------------------------------------------- */}
-                <div className='w-40 mr-4'>
-                    <div onClick={() => setshowSourceAvailability(true)} className='flex justify-between w-full px-1 text-sm border border-black border-solid cursor-pointer select-none'><div className='my-1'>Source & Availability</div><BsChevronDown className='my-auto' /></div>
-                    {showSourceAvailability &&
-                        <ClickOutsideAnElementHandler onClickedOutside={() => setshowSourceAvailability(false)}>
-                            <div className='absolute z-50 px-4 py-6 border border-black border-solid w-400px bg-cream'>
-                                <div className='flex'>
-                                    <div className='w-1/2'>
-                                        <div className='text-sm font-semibold font-ssp'>Source</div>
-                                        {sourceOptions.every(each => source.includes(each)) ?
-                                            <div onClick={() => setsource([])} className='mt-2 text-sm cursor-pointer select-none font-ssp text-link w-max'>
-                                                Unselect All
-                                            </div>
-                                            :
-                                            <div onClick={() => setsource(sourceOptions)} className='mt-2 text-sm cursor-pointer select-none font-ssp text-link w-max'>
-                                                Select All
-                                            </div>}
-                                        {sourceOptions.map(eachSource => <Checkbox label={eachSource} className='mt-4 text-sm text-secondary' checked={source.includes(eachSource)} onChange={(checked) => {
-                                            if (checked) {
-                                                setsource(state => state.concat(eachSource))
-                                            } else {
-                                                setsource(state => state.filter(each => each !== eachSource))
-                                            }
-                                        }} />)}
-                                    </div>
-                                    <div className='w-1/2 dropdown-list-input-overwrite'>
-                                        <div className='text-sm font-semibold font-ssp'>Collection</div>
-                                        {availabilityOptions.every(each => availability.includes(each)) ?
-                                            <div onClick={() => setavailability([])} className='mt-2 text-sm cursor-pointer select-none font-ssp text-link w-max'>
-                                                Unselect All
-                                            </div>
-                                            :
-                                            <div onClick={() => setavailability(availabilityOptions)} className='mt-2 text-sm cursor-pointer select-none font-ssp text-link w-max'>
-                                                Select All
-                                            </div>}
-                                        {availabilityOptions.map(eachAvailability => <Checkbox label={eachAvailability} className='mt-4 text-sm text-secondary' checked={availability.includes(eachAvailability)} onChange={(checked) => {
-                                            if (checked) {
-                                                setavailability(state => state.concat(eachAvailability))
-                                            } else {
-                                                setavailability(state => state.filter(each => each !== eachAvailability))
-                                            }
-                                        }} />)}
-                                    </div>
-                                </div>
-                                <div className='flex mt-8 mb-2'>
-                                    <Button onClick={() => setshowSourceAvailability(false)} variant='secondary' className='w-24 ml-auto mr-4'>Cancel</Button>
-                                    <Button className='w-24'>Apply</Button>
-                                </div>
-                            </div>
-                        </ClickOutsideAnElementHandler>
-                    }
-                </div>
+                <CatalogueFilterSource
+                showSourceAvailability={showSourceAvailability}
+                setshowSourceAvailability={setshowSourceAvailability}
+                source={source}
+                setsource={setsource}
+                availability={availability}
+                setavailability={setavailability}
+                />
                 {/**---------------------------------------price--------------------------------------------------------- */}
                 <div className='w-1/6 mr-4'>
                     <div onClick={() => setshowPrice(true)} className='flex justify-between w-full px-1 text-sm border border-black border-solid cursor-pointer select-none'><div className='my-1'>Price</div><BsChevronDown className='my-auto' /></div>
