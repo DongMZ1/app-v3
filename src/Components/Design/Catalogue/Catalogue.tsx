e36a0d398f5a5d803d3b122b91f367e7137c85a9
DongMZ1 <915094594@qq.com>
2022-03-01 14:43:51 -0500

    resolve fetch mutiply time bug on filter

--- src/Components/Design/Catalogue/Catalogue.tsx
+++ src/Components/Design/Catalogue/Catalogue.tsx
@@ -13,19 +13,21 @@ type CatalogueProps = {
 const Catalogue = ({ tabState }: CatalogueProps) => {
     const [calaloguePage, setcalaloguePage] = useState(1);
     const dispatch = useDispatch();
+    const quoteDetail = useSelector((state: Tappstate) => state.quoteDetail)
     const currency = useSelector((state: Tappstate) => state.quoteDetail?.currency);
     const products = useSelector((state: Tappstate) => state.products);
     const scrollRef = useRef<any>();
     const filterCatalogue = useSelector((state: Tappstate) => state.filterCatalogue);
     useEffect(() => {
+        if(quoteDetail){
         fetchProducts();
-        document.getElementById('catalogue-product-ref')?.animate({ scrollTop: 0 });
-    }, [currency, JSON.stringify(filterCatalogue)]);
+        }
+    }, [quoteDetail, JSON.stringify(filterCatalogue)]);
 
     const fetchMoreProducts = debouncePromise(async () => {
           if((scrollRef.current?.clientHeight + scrollRef.current?.scrollTop + 5) > scrollRef.current?.scrollHeight){
             const res = await apiRequest({
-                url: `/api/products-service/products/${currency ? currency : 'CAD'}?page=${calaloguePage}&limit=20`,
+                url: `/api/products-service/products/${currency ? currency : 'CAD'}?page=${calaloguePage}&limit=20${filterCatalogue?.minPrice !== undefined? `&priceMin=${Number(filterCatalogue?.minPrice)}`: ``}${filterCatalogue?.maxPrice? `&priceMax=${Number(filterCatalogue?.maxPrice)}`:''}${filterCatalogue?.nameOrSKU? `&nameOrSKU=${filterCatalogue?.nameOrSKU}`:''}${filterCatalogue?.color? `&colorName=${filterCatalogue?.color}`:''}${filterCatalogue?.maxWeight? `&weight=${filterCatalogue?.maxWeight}`:''}${filterCatalogue?.weightUnit? `&weightUnit=${filterCatalogue?.weightUnit}`:''}${filterCatalogue?.lengthUnit? `&dimensionUnit=${filterCatalogue?.lengthUnit}`:''}${filterCatalogue?.L? `&length=${filterCatalogue?.L}`:''}${filterCatalogue?.W? `&width=${filterCatalogue?.W}`:''}${filterCatalogue?.H? `&height=${filterCatalogue?.H}`:''}`,
                 method: 'GET'
             })
             if(res?.success){
@@ -39,7 +41,7 @@ const Catalogue = ({ tabState }: CatalogueProps) => {
     }, 1000, { leading: true });
     const fetchProducts = async () => {
         const res = await apiRequest({
-            url: `/api/products-service/products/${currency ? currency : 'CAD'}?page=0&limit=20${filterCatalogue?.minPrice !== undefined? `&priceMin=${Number(filterCatalogue?.minPrice)}`: ``}${filterCatalogue?.maxPrice? `&priceMax=${Number(filterCatalogue?.maxPrice)}`:''}${filterCatalogue?.nameOrSKU? `&nameOrSKU=${filterCatalogue?.nameOrSKU}`:''}${filterCatalogue?.color? `&colorName=${filterCatalogue?.color}`:''}${filterCatalogue?.maxWeight? `&weight=${filterCatalogue?.maxWeight}`:''}${filterCatalogue?.weightUnit? `&weightUnit=${filterCatalogue?.weightUnit}`:''}${filterCatalogue?.lengthUnit? `&dimensionUnit=${filterCatalogue?.lengthUnit}`:''}`,
+            url: `/api/products-service/products/${currency ? currency : 'CAD'}?page=0&limit=20${filterCatalogue?.minPrice !== undefined? `&priceMin=${Number(filterCatalogue?.minPrice)}`: ``}${filterCatalogue?.maxPrice? `&priceMax=${Number(filterCatalogue?.maxPrice)}`:''}${filterCatalogue?.nameOrSKU? `&nameOrSKU=${filterCatalogue?.nameOrSKU}`:''}${filterCatalogue?.color? `&colorName=${filterCatalogue?.color}`:''}${filterCatalogue?.maxWeight? `&weight=${filterCatalogue?.maxWeight}`:''}${filterCatalogue?.weightUnit? `&weightUnit=${filterCatalogue?.weightUnit}`:''}${filterCatalogue?.lengthUnit? `&dimensionUnit=${filterCatalogue?.lengthUnit}`:''}${filterCatalogue?.L? `&length=${filterCatalogue?.L}`:''}${filterCatalogue?.W? `&width=${filterCatalogue?.W}`:''}${filterCatalogue?.H? `&height=${filterCatalogue?.H}`:''}`,
             method: 'GET'
         })
         if (res.success) {
@@ -49,6 +51,7 @@ const Catalogue = ({ tabState }: CatalogueProps) => {
                 payload: res?.data?.products
             })
         }
+        document.getElementById('catalogue-product-ref')?.animate({ scrollTop: 0 });
     }
     return <div className={`${tabState !== "Catalogue" && 'catalogue-display-none-important'} flex h-full catalogue`}>
         <div className="flex flex-col w-1/2 overflow-hidden border-r border-black border-solid">
