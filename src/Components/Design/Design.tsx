bae618b499b1e28a653b67825bb05b53ec6f2ac9
DongMZ1 <915094594@qq.com>
2022-03-16 12:33:09 -0400

    sync with remote when go to design component

--- src/Components/Design/Design.tsx
+++ src/Components/Design/Design.tsx
@@ -4,9 +4,10 @@ import { Tab, TabProps } from '@fulhaus/react.ui.tab'
 import { IoMdBook } from "react-icons/io";
 import { FaRegListAlt } from "react-icons/fa";
 import Catalogue from './Catalogue/Catalogue'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Tappstate } from '../../redux/reducers';
 import apiRequest from '../../Service/apiRequest';
+import { getQuoteDetailAndUpdateSelectedUnit } from '../../redux/Actions';
 const tabs = [
   { icon: <FaRegListAlt style={{ marginRight: "0.5rem" }} />, label: "Catalogue" },
   { icon: <IoMdBook style={{ marginRight: "0.5rem" }} />, label: "Canvas" },
@@ -15,6 +16,9 @@ const Design = () => {
   const [tabState, settabState] = useState("Catalogue");
   const selectedProject = useSelector((state: Tappstate) => state.selectedProject);
   const currentOrgID = useSelector((state: Tappstate) => state.currentOrgID);
+  const quoteID = useSelector((state: Tappstate) => state.quoteDetail?._id);
+  const selectedQuoteUnit = useSelector((state: Tappstate) => state.selectedQuoteUnit)
+  const dispatch = useDispatch()
   useEffect(() => {
     const fetchDesign = async () => {
       const res = await apiRequest({
@@ -26,6 +30,16 @@ const Design = () => {
       fetchDesign()
     }
   }, [selectedProject, currentOrgID])
+  useEffect(() => {
+    //sync with remote, because previously custom items does not have a ID, so by fetch get single quote, it will have a ID now
+    if (currentOrgID && quoteID) {
+      dispatch(getQuoteDetailAndUpdateSelectedUnit({
+        organizationID: currentOrgID,
+        quoteID: quoteID,
+        selectedQuoteUnitID: selectedQuoteUnit?.unitID
+      }))
+    }
+  }, [currentOrgID, quoteID])
   return (
     <div className="w-full design">
       <div className="flex justify-center py-2 border-b border-black border-solid justify-items-center h-14 tab-css-overwrite">
