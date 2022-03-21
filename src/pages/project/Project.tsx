8cf2d20eaa225d99e8a2d334b5f91eebb7381956
DongMZ1 <915094594@qq.com>
2022-03-21 12:41:36 -0400

    lock quote functionality finished

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -24,8 +24,7 @@ import AppSideBar from '../../Components/AppSideBar/AppSideBar';
 import QuoteSummaryPurchase from '../../Components/QuoteSummaryPurchase/QuoteSummaryPurchase';
 import QuoteSummaryRental from '../../Components/QuoteSummaryRental/QuoteSummaryRental';
 import SaveProject from './ProjectComponents/SaveProject';
-import {ImUnlocked} from 'react-icons/im'
-import {FaLock} from 'react-icons/fa'
+import LockQuote from './ProjectComponents/LockQuote';
 const Project = () => {
     const [showInvitePeople, setshowInvitePeople] = useState(false);
     const [showHistory, setshowHistory] = useState(false);
@@ -34,6 +33,7 @@ const Project = () => {
     const [showConfirmDeleteProjectModal, setshowConfirmDeleteProjectModal] = useState(false);
     const [hoverProjectTitle, sethoverProjectTitle] = useState(false);
     const selectedProject = useSelector((state: Tappstate) => state.selectedProject);
+    const quoteDetail = useSelector((state: Tappstate) => state?.quoteDetail);
     const userRole = useSelector((state: Tappstate) => state.selectedProject)?.userRole;
     const currentOrgID = useSelector((state: Tappstate) => state.currentOrgID);
     const [projectTitle, setprojectTitle] = useState(selectedProject?.title)
@@ -195,9 +195,7 @@ const Project = () => {
                     {userRole !== 'viewer' && <>
                         <HistoryIcon onClick={() => setshowHistory(true)} className='my-auto mr-8 cursor-pointer' />
                         <SaveProject /></>}
-                    {
-                        userRole !== 'viewer' && userRole !== 'editor' && (window.location.href.includes('/quote-only') || window.location.href.includes('/project/quote')) && <ImUnlocked className='my-auto mr-8 cursor-pointer' />
-                    }
+                    <LockQuote />
                 </div>
             </div>
             <CSSTransition in={showHistory} timeout={300} unmountOnExit classNames='opacity-animation'><VersionHistory close={() => setshowHistory(false)} /></CSSTransition>
