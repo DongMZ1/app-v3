6a876b8d72c26bbfc7de5541c5b00c797ee07822
DongMZ1 <915094594@qq.com>
2022-03-29 14:08:07 -0400

    canvas fixed

--- src/Components/Design/Canvas/Canvas.tsx
+++ src/Components/Design/Canvas/Canvas.tsx
@@ -126,23 +126,7 @@ const Canvas = () => {
                 )
             }
         </div>
-        <DesignCanvas designItems={[
-            {
-                type: "image",
-                name: "Pedro Mid-century Dining Table Pedro Mid-century Dining Table",
-                value: "https://dl.airtable.com/.attachments/dc4a9a9663b09fbc526cb3e5de67c0be/31ff5715/Imagefromsafavieh.comon2022-03-07at4.26.25PM.jpeg",
-            },
-            {
-                type: "image",
-                name: "Lucius Outdoor Stool",
-                value: "https://files.plytix.com/api/v1.1/file/public_files/pim/assets/a1/ae/d4/5c/5cd4aea1a3dec0046811d88f/images/55/24/f8/5c/5cf82455e5e4ad046c385538/BQ-1006-25.jpg",
-            },
-            {
-                type: "image",
-                name: "Vintage Bench Large Grey",
-                value: "https://files.plytix.com/api/v1.1/file/public_files/pim/assets/a1/ae/d4/5c/5cd4aea1a3dec0046811d88f/images/ac/26/f8/5c/5cf826acde06fd046b578d68/BT-1001-37.jpg",
-            },
-        ]} />
+        <DesignCanvas designItems={designItems?.length > 0 ? designItems : []} />
     </div>
 }
 
