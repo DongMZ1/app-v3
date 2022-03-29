3192a966999b15ab5ad3efc97020df3ac12a6063
DongMZ1 <915094594@qq.com>
2022-03-29 12:12:14 -0400

    delete unit package finished

--- src/Components/AppSideBar/AppSideBar.tsx
+++ src/Components/AppSideBar/AppSideBar.tsx
@@ -11,7 +11,7 @@ import { SwitchTransition, CSSTransition, Transition } from 'react-transition-gr
 import { TextInput } from '@fulhaus/react.ui.text-input';
 import { Checkbox } from '@fulhaus/react.ui.checkbox'
 import { Button } from '@fulhaus/react.ui.button'
-import { ClickOutsideAnElementHandler } from '@fulhaus/react.ui.click-outside-an-element-handler';
+import {showMessageAction} from '../../redux/Actions'
 import { Loader } from '@fulhaus/react.ui.loader'
 import { RiDeleteBin5Line } from 'react-icons/ri'
 import { ActionModal } from '@fulhaus/react.ui.action-modal';
@@ -92,7 +92,16 @@ const AppSideBar = () => {
     }
 
     const deleteRoomPackage = async () => {
-        
+        const res = await apiRequest({
+            url: `/api/fhapp-service/package/unit/${currentOrgID}/${selectedUnitToDelete?.id}`,
+            method: 'DELETE'
+        });
+        if (res?.success) {
+           await getUnitPackages();
+           setshowSelectedUnitToDelete(false);
+        }else{
+            dispatch(showMessageAction(true, res?.message));
+        }
     }
     return (<>
         <ActionModal modalClassName='font-moret' showModal={showSelectedUnitToDelete} message={`Delete Unit Package => ${selectedUnitToDelete?.name}`} subText={`Are you sure you want to permanently delete unit package ${selectedUnitToDelete?.name} ?`} onCancel={() => setshowSelectedUnitToDelete(false)} submitButtonLabel={'Delete'} cancelButtonLabel={'Cancel'} onSubmit={() => deleteRoomPackage()} />
@@ -108,49 +117,47 @@ const AppSideBar = () => {
                         <div className='my-auto mr-4 text-sm font-ssp'>add:</div>
                         <div className='relative w-20 text-sm-important'>
                             <div onClick={() => setshowAddUnitDropdown(true)} className='flex w-full h-8 border border-black border-solid cursor-pointer hover:bg-black hover:border-transparent hover:text-white'><div className='my-auto ml-auto mr-1'>Units</div><AiOutlineDown className='my-auto mr-auto' /></div>
-                            <ClickOutsideAnElementHandler onClickedOutside={() => setshowAddUnitDropdown(false)}>
-                                <CSSTransition in={showAddUnitDropdown} timeout={300} unmountOnExit classNames='height-800px-animation'>
-                                    <div className='absolute z-50 p-4 overflow-y-auto bg-white border border-black border-solid w-96'>
+                            <CSSTransition in={showAddUnitDropdown} timeout={300} unmountOnExit classNames='height-800px-animation'>
+                                <div className='absolute z-50 p-4 overflow-y-auto bg-white border border-black border-solid w-96'>
 
-                                        <div className='text-sm font-semibold font-ssp'>
-                                            New Unit
-                                        </div>
-                                        <TextInput placeholder='Enter Unit Name' variant='box' className='mt-2' inputName='customUnitName' value={customUnitName} onChange={(e) => setcustomUnitName((e.target as any).value)} />
-                                        <div className='mt-4 text-sm font-semibold font-ssp'>
-                                            Choose an existing unit package
-                                        </div>
-                                        <TextInput placeholder='Search existing unit packages' variant='box' className='mt-2' inputName='unit package keywords' value={unitPackageKeyword} onChange={(e) => {
-                                            setunitPackageKeyword((e.target as any).value);
-                                            setunitOptionCheckedList([]);
-                                        }}
-                                        />
-                                        <div className='w-full overflow-y-auto max-h-60'>
-                                            {unitOptionList?.filter(eachUnit => eachUnit?.name.toLowerCase().includes(unitPackageKeyword.toLowerCase())).map(each =>
-                                                <div className='flex my-2'>
-                                                    <Checkbox label={each?.name} checked={unitOptionCheckedList.includes(each)} onChange={(v) => {
-                                                        if (v) {
-                                                            setunitOptionCheckedList(state => [...state, each])
-                                                        } else {
-                                                            setunitOptionCheckedList(state => state.filter(e => e !== each))
-                                                        }
-                                                    }} />{(userRole === 'admin' || userRole === 'owner') && <RiDeleteBin5Line
-                                                        onClick={() => {
-                                                            setselectedUnitToDelete(each);
-                                                            setshowSelectedUnitToDelete(true);
-                                                        }}
-                                                        className='my-auto ml-auto mr-4 cursor-pointer' color='red' />}</div>)}
-                                        </div>
-                                        <div className='flex my-2'>
-                                            <Button onClick={() => {
-                                                setshowAddUnitDropdown(false)
-                                            }} className='w-32 mr-4' variant='secondary'>Cancel</Button>
-                                            <Button disabled={unitOptionCheckedList.length === 0 && customUnitName === ''} onClick={() => {
-                                                createUnits();
-                                            }} variant='primary' className='w-32'>Create Units</Button>
-                                        </div>
+                                    <div className='text-sm font-semibold font-ssp'>
+                                        New Unit
                                     </div>
-                                </CSSTransition>
-                            </ClickOutsideAnElementHandler>
+                                    <TextInput placeholder='Enter Unit Name' variant='box' className='mt-2' inputName='customUnitName' value={customUnitName} onChange={(e) => setcustomUnitName((e.target as any).value)} />
+                                    <div className='mt-4 text-sm font-semibold font-ssp'>
+                                        Choose an existing unit package
+                                    </div>
+                                    <TextInput placeholder='Search existing unit packages' variant='box' className='mt-2' inputName='unit package keywords' value={unitPackageKeyword} onChange={(e) => {
+                                        setunitPackageKeyword((e.target as any).value);
+                                        setunitOptionCheckedList([]);
+                                    }}
+                                    />
+                                    <div className='w-full overflow-y-auto max-h-60'>
+                                        {unitOptionList?.filter(eachUnit => eachUnit?.name.toLowerCase().includes(unitPackageKeyword.toLowerCase())).map(each =>
+                                            <div className='flex my-2'>
+                                                <Checkbox label={each?.name} checked={unitOptionCheckedList.includes(each)} onChange={(v) => {
+                                                    if (v) {
+                                                        setunitOptionCheckedList(state => [...state, each])
+                                                    } else {
+                                                        setunitOptionCheckedList(state => state.filter(e => e !== each))
+                                                    }
+                                                }} />{(userRole === 'admin' || userRole === 'owner') && <RiDeleteBin5Line
+                                                    onClick={() => {
+                                                        setselectedUnitToDelete(each);
+                                                        setshowSelectedUnitToDelete(true);
+                                                    }}
+                                                    className='my-auto ml-auto mr-4 cursor-pointer' color='red' />}</div>)}
+                                    </div>
+                                    <div className='flex my-2'>
+                                        <Button onClick={() => {
+                                            setshowAddUnitDropdown(false)
+                                        }} className='w-32 mr-4' variant='secondary'>Cancel</Button>
+                                        <Button disabled={unitOptionCheckedList.length === 0 && customUnitName === ''} onClick={() => {
+                                            createUnits();
+                                        }} variant='primary' className='w-32'>Create Units</Button>
+                                    </div>
+                                </div>
+                            </CSSTransition>
                         </div>
                         <div className='my-auto ml-auto cursor-pointer' onClick={() => setshowEntendSideBar(false)}>
                             <AiOutlineLeft size={22} />
