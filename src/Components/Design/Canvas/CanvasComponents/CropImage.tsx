7fb25f371e8ee32e531c90d85e64f8c336394b58
DongMZ1 <915094594@qq.com>
2022-04-22 20:14:16 -0400

    network bug for tainted image

--- src/Components/Design/Canvas/CanvasComponents/CropImage.tsx
+++ src/Components/Design/Canvas/CanvasComponents/CropImage.tsx
@@ -10,7 +10,7 @@ type TCropImage = {
     cropImageDesignItems: TDesignItem[]
     onClose: () => void
 }
-function getCroppedImg(image: any, crop: any, fileName: any) {
+function getCroppedImg(image: HTMLImageElement, crop: any, fileName: any) {
     const canvas = document.createElement('canvas');
     const scaleX = image.naturalWidth / image.width;
     const scaleY = image.naturalHeight / image.height;
@@ -30,31 +30,30 @@ function getCroppedImg(image: any, crop: any, fileName: any) {
         crop.height
     );
 
-    return new Promise((resolve, reject) => {
-        canvas.toBlob((blob: any) => {
-            if (!blob) {
-                //reject(new Error('Canvas is empty'));
-                console.error('Canvas is empty');
-                return;
-            }
-            blob.name = fileName;
-            blob.lastModifiedDate = new Date();
-            let fileUrl = URL.createObjectURL(blob);
-            resolve(blob);
-        }, 'image/jpeg');
-    });
+    const base64Canvas = canvas.toDataURL("image/jpeg").split(';base64,')[1]
+    return base64Canvas;
+
+    // return new Promise((resolve, reject) => {
+    //     canvas.toBlob((blob: any) => {
+    //         if (!blob) {
+    //             //reject(new Error('Canvas is empty'));
+    //             console.error('Canvas is empty');
+    //             return;
+    //         }
+    //         blob.name = fileName;
+    //         blob.lastModifiedDate = new Date();
+    //         // let fileUrl = URL.createObjectURL(blob);
+    //         resolve(blob);
+    //     }, 'image/jpeg');
+    // });
 }
 
 const CropImage = ({ cropImageID, cropImageDesignItems, onClose }: TCropImage) => {
     const [crop, setCrop] = useState<Crop>()
     const imgRef = useRef(null);
-    const onImageLoaded = (image: any) => {
-        imgRef.current = image;
-        console.log(JSON.stringify(image))
-    };
 
     const cropImage = async () => {
-        const blob: any = await getCroppedImg(imgRef.current, crop, 'cropped-image.jpeg');
+        const blob: any = await getCroppedImg(imgRef.current as any, crop, 'cropped-image.jpeg');
         console.log(blob)
     }
 
@@ -63,12 +62,12 @@ const CropImage = ({ cropImageID, cropImageDesignItems, onClose }: TCropImage) =
             crop={crop}
             onChange={(c => setCrop(c))}
         >
-            <img className='height-70vh' alt='croped-img' src={cropImageDesignItems.filter(each => each.id === cropImageID)?.[0]?.value} />
+            <img ref={imgRef} crossOrigin='anonymous' className='height-70vh' alt='croped-img' src={cropImageDesignItems.filter(each => each.id === cropImageID)?.[0]?.value} />
         </ReactCrop>
         <div className='flex w-full'>
             <div className='w-1/2 my-auto text-sm font-semibold text-red font-ssp'>Warning: Once the image is cropped, you are not able to restore it or remove its background.</div>
             <Button variant='secondary' onClick={onClose} className='w-24 ml-auto mr-8'>Cancel</Button>
-            <Button className='w-24' onClick={() => {}}>Crop</Button>
+            <Button className='w-24' onClick={() => cropImage()}>Crop</Button>
         </div>
     </div>
 }
